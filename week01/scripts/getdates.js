f63ffe9d1794535135ad9ed41c70c74e07051d64
ThalesLuizCosta <thalesluizc@gmail.com>
2024-09-27 23:47:34 -0300

    I modified all correctly

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,5 +1,3 @@
-// Get the current year and update the footer
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Get the last modified date of the document and update the footer
-document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;

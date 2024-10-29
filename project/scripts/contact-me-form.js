89bca554c3a670d62472eb58e6dde18df653b77b
ThalesLuizCosta <thalesluizc@gmail.com>
2024-10-29 00:09:34 -0300

    I finish it finally!

--- project/scripts/contact-me-form.js
+++ project/scripts/contact-me-form.js
@@ -1,4 +1,13 @@
-const currentYear = document.getElementById('currentyear');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById("menu");
+    const navigation = document.querySelector('.navigation');
+
+    menuButton.addEventListener('click', function() {
+        navigation.classList.toggle('open'); // Alterna a classe 'open' para mostrar ou esconder a navegação
+    });
+});
+
+const currentYear = document.getElementById('currentYear');
 currentYear.textContent = new Date().getFullYear();
 
 const lastModified = document.getElementById('lastModified');
@@ -6,14 +15,6 @@ lastModified.textContent = (document.lastModified);
 
 const selectElement = document.querySelector("#productName");
 const form = document.querySelector("form");
-
-products.forEach(option => {
-    let newOption = document.createElement("option");
-    newOption.value = option.id;
-    newOption.text = option.name;
-
-    selectElement.appendChild(newOption);
-});
 
 let counter = getCount() || 0;
 
